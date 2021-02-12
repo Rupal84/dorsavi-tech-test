1134b6274bf70a3548710475111bae9ac98d90b6
Kulwinder Rupal <Kulwinder_Rupal@yahoo.com>
2021-02-12 19:20:37 +1100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import reactDOM from 'react-dom';
+import './styles/styles.scss';
+
+const App = () => {
+    return (
+        <h1 className="red">headerdd</h1>
+    )
+}
+
+reactDOM.render(<App />, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            }
+        ]
+    },
+    devtool: 'cheap-module-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'public')
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        })
+    ]
+}
