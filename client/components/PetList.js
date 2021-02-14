bd335075a6454f0461d8e5381b7370d17568cf6b
Kulwinder Rupal <Kulwinder_Rupal@yahoo.com>
2021-02-14 18:03:06 +1100

    Updated ui

--- client/components/PetList.js
+++ client/components/PetList.js
@@ -1,24 +1,46 @@
 import React from 'react';
 import query from '../queries/fetchOwnersAndPets';
-import { useQuery, graphql } from '@apollo/client';
+import { useQuery } from '@apollo/client';
 
 const PetList = (props) => {
+    const {petName, city, petType} = props;
     const {loading, data} = useQuery(query, {
         variables: { petName, city, petType },
       }); 
     const extractList = ({owners}) => {
-        return owners.map(({pets}) => {
+        const males = owners.filter((owner)=>owner.gender === 'Male');
+        const females = owners.filter((owner)=>owner.gender === 'Female');
+        const malesList = males.map((male) => {
+            const {pets} = male
            return pets && pets.map((pet) => {
-                return <li>{pet.name}</li>
+                return <li key={pet.name}>{pet.name} - {male.city}</li>
             })
         })
+        const femalesList = females.map((female) => {
+            const {pets} = female;
+            return pets && pets.map((pet) => {
+                 return <li key={pet.name}>{pet.name} - {female.city}</li>
+             })
+         })
+
+         return (
+             <div>
+                 <h3>Male</h3>
+                 <ul key='males'>
+                     {malesList}
+                 </ul>
+                 <h3>Female</h3>
+                 <ul key='females'>
+                     {femalesList}
+                 </ul>
+             </div>
+         )
     }
     if (loading) { return <div>Loading...</div>; }
     return (
-       
-        <ul>
+        <div>
             {extractList(data)}
-        </ul>
+        </div>
     )
 }
 
--- client/queries/fetchOwnersAndPets.js
+++ client/queries/fetchOwnersAndPets.js
@@ -8,7 +8,8 @@ export default gql`
       pets {
         name,
         type
-      }
+      },
+      city
     }
   }
 `;
